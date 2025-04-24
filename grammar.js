9e13906bb07a117d3a299196a882e96058cc404f
Nicolás de Camino <ndecamino@uc.cl>
2025-04-24 00:53:57 -0400

    Initial parser

--- grammar.js
+++ grammar.js
@@ -1,17 +1,134 @@
-/**
- * @file Stata grammar for tree-sitter
- * @author Nicolas de Camino <ndecamino@uc.cl>
- * @license MIT
- */
-
-/// <reference types="tree-sitter-cli/dsl" />
-// @ts-check
-
 module.exports = grammar({
-  name: "stata",
+  name: 'stata',
+
+  // Specify precedence for conflict resolution
+  precedences: $ => [
+    ['command', 'macro'],
+    ['options', 'qualifier']
+  ],
+  
+  conflicts: $ => [
+    [$._command_with_args, $.stata_command],
+    [$.varlist, $.macro_reference],
+    [$.option, $.options]
+  ],
+
+  extras: $ => [
+    $.comment,
+    /\s/
+  ],
 
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    source_file: $ => repeat($._statement),
+
+    _statement: $ => choice(
+      $.command,
+      $.control_flow_structure,
+      $.macro_definition,
+      $.comment,
+      $.empty_statement
+    ),
+
+    empty_statement: $ => ';',
+
+    macro_definition: $ => prec.right('macro', seq('$', $.identifier, '=', $.expression)),
+
+    macro_reference: $ => prec('macro', seq('$', $.identifier)),
+    
+    identifier: $ => /[a-zA-Z_][a-zA-Z0-9_]*/,
+    
+    command: $ => prec.left('command', seq(
+      optional(seq('by', $.varlist, ':')),
+      $._command_with_args
+    )),
+
+    _command_with_args: $ => prec.left(seq(
+      $.stata_command,
+      optional($.varlist),
+      optional(seq('=', $.expression)),
+      repeat(choice(
+        $.if_qualifier,
+        $.in_qualifier,
+        $.weight_qualifier,
+        $.options_qualifier
+      ))
+    )),
+
+    if_qualifier: $ => prec('qualifier', seq('if', $.expression)),
+    
+    in_qualifier: $ => prec('qualifier', seq('in', $.range)),
+    
+    weight_qualifier: $ => prec('qualifier', seq('weight', $.expression)),
+    
+    options_qualifier: $ => prec('qualifier', seq(',', prec.left('options', $.options))),
+
+    stata_command: $ => $.identifier,
+
+    varlist: $ => prec.left(repeat1(choice(
+      $.identifier,
+      $.macro_reference
+    ))),
+    
+    comment: $ => token(choice(
+      seq('//', /.*/),
+      seq('/*', /[^*]*\*+([^/*][^*]*\*+)*/, '/')
+    )),
+
+    control_flow_structure: $ => choice(
+      $.if_structure,
+      $.for_structure
+    ),
+
+    if_structure: $ => seq(
+      'if', 
+      $.expression, 
+      '{', 
+      repeat($._statement), 
+      '}',
+      optional(seq('else', '{', repeat($._statement), '}'))
+    ),
+
+    for_structure: $ => seq(
+      'forvalues', 
+      $.identifier,
+      '=',
+      $.range,
+      '{', 
+      repeat($._statement), 
+      '}'
+    ),
+
+    expression: $ => choice(
+      $.identifier,
+      $.number,
+      $.string,
+      $.macro_reference,
+      $.binary_expression
+    ),
+
+    binary_expression: $ => prec.left(1, seq(
+      field('left', $.expression),
+      field('operator', choice('>', '<', '>=', '<=', '==', '!=', '+', '-', '*', '/')),
+      field('right', $.expression)
+    )),
+
+    number: $ => /[0-9]+(\.[0-9]+)?/,
+    
+    string: $ => choice(
+      seq('"', /[^"]*/, '"'),
+      seq('\'', /[^\']*/, '\'')
+    ),
+
+    range: $ => /\d+\s*(-|\/)\s*\d+/,
+
+    options: $ => prec.left('options', seq(
+      $.option,
+      repeat(seq(',', $.option))
+    )),
+
+    option: $ => choice(
+      $.identifier,
+      seq($.identifier, '(', $.expression, ')')
+    )
   }
 });
